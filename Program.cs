426fb541787826004ccfa1b052b3893b139f0a1d
kntjsprr <65537922+kntjsprr@users.noreply.github.com>
2022-08-23 01:34:10 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.IO;
-using System.Text.RegularExpressions;
+
 namespace lzma
 {
     public partial class Program
@@ -11,51 +11,58 @@ namespace lzma
         static async Task Main(string[] args)
         {
             #region Initializing Variables
-            string encryptionKey, extension;
+
+            string encryptionKey, extension, fullPath, savePath, action;
             byte[] encryptionKeyBytes;
             CitadelMain.Mode conversionMode;
-            string defaultKey = "hkN!VEh+g=9rwxzZ";
+            string defaultKey = @"LLp53sQt%BDPmKn?";
+            // filePath and fileName used to split fullPath (Currently not used. Initially created to implement predetermined filenames.)
             #endregion
-            if (args.Length <= 0)
+
+
+            int pointer = args.Length - 1;
+            if (pointer < 0) //No argument
             {
                 return;
             }
             else if (args[0] == "-h")
             {
+             
                 return;
             }
 
-            if (args.Length < 2)
+            if (pointer < 1)
             {
                 return;
             }
-            else if (args.Length < 3)
+            else if (pointer < 2)
             {
                 return;
             }
-
-            string fullPath = @args[0]; string savePath = @args[1]; string action = @args[2];
+            fullPath = @args[0]; savePath = @args[1]; action = @args[2];
 
             if (!File.Exists(fullPath))
             {
+
                 return;
             }
             else if (!Directory.Exists(savePath))
             {
+                Console.Write("Argument " + savePath + "doesn't exist. \nUse -h for help.");
                 return;
             }
             switch (action)
             {
-                case "-e":
+                case @"-e":
                     conversionMode = CitadelMain.Mode.Transform;
                     break;
-                case "-d":
+                case @"-d":
                     conversionMode = CitadelMain.Mode.Restore;
                     break;
                 default:
                     return;
             }
-            if (args.Length < 3)
+            if (pointer < 3)
             {
                 encryptionKey = defaultKey;
             }
@@ -67,34 +74,44 @@ namespace lzma
             {
                 encryptionKey = args[3];
             }
-
-            if (args.Length < 4)
+            if (pointer < 4)
             {
-                extension = "enc"; 
+                extension = "enc"; //File extension of the encrypted file
             }
             else
             {
                 extension = @args[4];
             }
-            Regex rxPattern = new Regex(@"[^\\\\]*$");
-            Match rx = rxPattern.Match(fullPath); 
-            string filePath = Regex.Replace(fullPath, rx.Value, ""); 
+
+            /*
+            Regex rxPattern = new Regex(@"[^\\\\]*$"); // Regex pattern to use
+            Match rx = rxPattern.Match(fullPath); //Match fileName pattern to fullpath and put it into a variable named fileName
+            string filePath = Regex.Replace(fullPath, rx.Value, ""); //Remove fileName value and name it to a var named filePath
             string fileName = rx.Value;
+            */
+
             using (SHA256Managed SHA256 = new SHA256Managed())
-            encryptionKeyBytes = SHA256.ComputeHash(Encoding.UTF8.GetBytes(encryptionKey));
+                encryptionKeyBytes = SHA256.ComputeHash(Encoding.UTF8.GetBytes(encryptionKey));
             Task<string> task;
             string newFilePath = null;
+
             CitadelMain ft = new CitadelMain(encryptionKeyBytes);
             if (conversionMode == CitadelMain.Mode.Transform)
             {
                 task = Task.Run(() => ft.TransformFile(fullPath, savePath, extension));
             }
             else
-            { 
+            {  // (conversionMode == CitadelMain.Mode.Restore)
                 task = Task.Run(() => ft.RestoreFile(fullPath, savePath));
+
             }
             newFilePath = await task;
-            Console.Write(Path.GetFileName(newFilePath));
+
+
+
         }
+
+
     }
+
 }
